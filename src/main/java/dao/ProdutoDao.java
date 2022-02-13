22fc35200c7d6c893f09d329fb8f5bf63000fdab
Daniel Diniz <daniel.diniz1@icloud.com>
2022-02-13 13:24:01 -0300

    metodo alternativo com JPA puro para consultas dinamicas (WHERE 1=1)

--- src/main/java/dao/ProdutoDao.java
+++ src/main/java/dao/ProdutoDao.java
@@ -3,7 +3,9 @@ package dao;
 import modelo.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProdutoDao {
@@ -38,7 +40,6 @@ public class ProdutoDao {
 
     public List<Produto> buscarPorNome(String nome){
         String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
-
         return em.createQuery(jpql,Produto.class)
                 .setParameter("nome", nome)
                 .getResultList();
@@ -58,9 +59,33 @@ public class ProdutoDao {
 
     public BigDecimal buscarPrecoDoProdutoPorNome(String nome){
         String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
-
         return em.createQuery(jpql,BigDecimal.class)
                 .setParameter("nome", nome)
                 .getSingleResult();
+    }
+
+    public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro){
+        String jpql = "SELECT p FROM Produto p WHERE 1=1";
+        if (nome != null && !nome.trim().isEmpty()){
+            jpql += "AND p.nome = :nome";
+        }
+        if (preco != null){
+            jpql += "AND p.preco = :preco";
+        }
+        if (dataCadastro != null){
+            jpql += "AND p.dataCadastro = :dataCadastro";
+        }
+        TypedQuery<Produto> query = em.createQuery(jpql,Produto.class);
+        if (nome != null && !nome.trim().isEmpty()){
+            query.setParameter("nome", nome);
+        }
+        if (preco != null){
+            query.setParameter("preco", preco);
+        }
+        if (dataCadastro != null){
+            query.setParameter("dataCadastro",dataCadastro);
+        }
+
+        return query.getResultList();
     }
 }
